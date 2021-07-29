c7469bae82013bb5d3730d7f51e79f526c3d37e5
myeo1 <myeo@swin.edu.au>
2021-07-29 18:26:01 +1000

    test2

--- dbapp/urls.py
+++ dbapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import test_view, records
 
+# paths
 urlpatterns = [
     path("test_view/", test_view, name="test_view"),
     path("records/", records, name="records")
